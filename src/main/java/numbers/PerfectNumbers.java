cbad214aa46680e894aa7d3def1055d5847f2794
codriv <67503884+codriv@users.noreply.github.com>
2021-11-09 17:41:13 +0100

    method became public

--- src/main/java/numbers/PerfectNumbers.java
+++ src/main/java/numbers/PerfectNumbers.java
@@ -2,7 +2,7 @@ package numbers;
 
 public class PerfectNumbers {
 
-    boolean isPerfectNumber(int number) {
+    public boolean isPerfectNumber(int number) {
         int sumOfDivisors = 0;
         for ( int i = 1; i <= (number / 2); i++) {
             if (number % i == 0) {
