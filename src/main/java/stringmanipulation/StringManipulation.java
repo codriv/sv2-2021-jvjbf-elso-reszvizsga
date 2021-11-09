1c4b053e7da4cb7ee51ed5452f7a3d0663c3cf0b
codriv <combodriver4@yahoo.at>
2021-11-09 15:48:42 +0100

    exam-task-StringManipulation completed

---
+++ src/main/java/stringmanipulation/StringManipulation.java
@@ -0,0 +1,12 @@
+package stringmanipulation;
+
+public class StringManipulation {
+
+    String everyEvenCharacter(String s) {
+        StringBuilder filteredString = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2) {
+            filteredString.append(s.charAt(i));
+        }
+        return filteredString.toString();
+    }
+}
